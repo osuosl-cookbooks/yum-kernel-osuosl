780409598e0022801596498a34fcff11b1fbdc0b
Lance Albertson <lance@osuosl.org>
2018-03-01 14:06:31 -0800

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+yum-kernel-osuosl CHANGELOG
+===========================
+This file is used to list changes made in each version of the
+yum-kernel-osuosl cookbook.
+
+0.1.0
+-----
+- Initial release of yum-kernel-osuosl
+
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'yum-kernel-osuosl'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'apachev2'
+issues_url       'https://github.com/osuosl-cookbooks/yum-kernel-osuosl/issues'
+source_url       'https://github.com/osuosl-cookbooks/yum-kernel-osuosl'
+description      'Installs/Configures yum-kernel-osuosl'
+long_description 'Installs/Configures yum-kernel-osuosl'
+version          '0.1.0'
+
+supports         'centos', '~> 7.0'
