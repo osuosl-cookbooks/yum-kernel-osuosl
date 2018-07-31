06f3ea91ca19c6b8b1970cb8b13ec3cc4d9da334
Jason Chen <jasonc@osuosl.org>
2018-07-31 14:53:50 -0700

    Switch to cookstyle and apply fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,11 +3,9 @@ default['yum']['kernel-osuosl']['description'] = 'OSUOSL Linux Kernel'
 default['yum']['kernel-osuosl']['enabled'] = true
 default['yum']['kernel-osuosl']['gpgcheck'] = true
 default['yum']['kernel-osuosl']['gpgkey'] = 'https://ftp.osuosl.org/pub/osl/repos/yum/RPM-GPG-KEY-osuosl'
-case node['kernel']['machine']
-when 'ppc64le'
-  default['yum']['kernel-osuosl']['baseurl'] =
-    'https://ftp.osuosl.org/pub/osl/repos/yum/openpower/centos-$releasever/ppc64le/kernel-osuosl/'
-else
-  default['yum']['kernel-osuosl']['baseurl'] =
-    'https://ftp.osuosl.org/pub/osl/repos/yum/$releasever/kernel-osuosl/$basearch/'
-end
+default['yum']['kernel-osuosl']['baseurl'] = case node['kernel']['machine']
+                                             when 'ppc64le'
+                                               'https://ftp.osuosl.org/pub/osl/repos/yum/openpower/centos-$releasever/ppc64le/kernel-osuosl/'
+                                             else
+                                               'https://ftp.osuosl.org/pub/osl/repos/yum/$releasever/kernel-osuosl/$basearch/'
+                                             end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,23 +5,23 @@ ChefSpec::Coverage.start! { add_filter 'yum-kernel-osuosl' }
 
 CENTOS_7 = {
   platform: 'centos',
-  version: '7.2.1511'
+  version: '7.2.1511',
 }.freeze
 
 CENTOS_6 = {
   platform: 'centos',
-  version: '6.7'
+  version: '6.7',
 }.freeze
 
 DEBIAN_8 = {
   platform: 'debian',
-  version: '8.4'
+  version: '8.4',
 }.freeze
 
 ALL_PLATFORMS = [
   CENTOS_6,
   CENTOS_7,
-  DEBIAN_8
+  DEBIAN_8,
 ].freeze
 
 RSpec.configure do |config|
