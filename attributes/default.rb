b946db8f0543220a9d74e89a85ac52702b30786b
Jason Chen <jasonc@osuosl.org>
2018-08-02 11:30:52 -0700

    Move attribute case statement down a line

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,10 @@ default['yum']['kernel-osuosl']['description'] = 'OSUOSL Linux Kernel'
 default['yum']['kernel-osuosl']['enabled'] = true
 default['yum']['kernel-osuosl']['gpgcheck'] = true
 default['yum']['kernel-osuosl']['gpgkey'] = 'https://ftp.osuosl.org/pub/osl/repos/yum/RPM-GPG-KEY-osuosl'
-default['yum']['kernel-osuosl']['baseurl'] = case node['kernel']['machine']
-                                             when 'ppc64le'
-                                               'https://ftp.osuosl.org/pub/osl/repos/yum/openpower/centos-$releasever/ppc64le/kernel-osuosl/'
-                                             else
-                                               'https://ftp.osuosl.org/pub/osl/repos/yum/$releasever/kernel-osuosl/$basearch/'
-                                             end
+default['yum']['kernel-osuosl']['baseurl'] =
+  case node['kernel']['machine']
+  when 'ppc64le'
+    'https://ftp.osuosl.org/pub/osl/repos/yum/openpower/centos-$releasever/ppc64le/kernel-osuosl/'
+  else
+    'https://ftp.osuosl.org/pub/osl/repos/yum/$releasever/kernel-osuosl/$basearch/'
+  end
