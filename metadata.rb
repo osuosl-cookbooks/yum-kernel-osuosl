fc3477d4eecf28d011c747e7b0b9f9ea835d501f
Lance Albertson <lance@osuosl.org>
2020-06-04 15:53:51 -0700

    Cookstyle fixes

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ issues_url       'https://github.com/osuosl-cookbooks/yum-kernel-osuosl/issues'
 license          'Apache-2.0'
 chef_version     '>= 14.0'
 description      'Installs/Configures yum-kernel-osuosl'
-long_description 'Installs/Configures yum-kernel-osuosl'
 version          '2.1.0'
 
 supports         'centos', '~> 7.0'
