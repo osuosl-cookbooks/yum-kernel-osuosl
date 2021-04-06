d1f5eda6f174317c210b0c54b14e10a460aae61e
Ryan <kennedy@osuosl.org>
2021-04-06 09:14:40 -0700

    Update Chef dependency to >= 16

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@osuosl.org'
 source_url       'https://github.com/osuosl-cookbooks/yum-kernel-osuosl'
 issues_url       'https://github.com/osuosl-cookbooks/yum-kernel-osuosl/issues'
 license          'Apache-2.0'
-chef_version     '>= 14.0'
+chef_version     '>= 16.0'
 description      'Installs/Configures yum-kernel-osuosl'
 version          '2.1.2'
 
